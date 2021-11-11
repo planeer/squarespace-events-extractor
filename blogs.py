2377ef2e4d27ee9b666659fb99f01dd4ddf96127
Nejc Planer <nejcplaner@gmail.com>
2021-11-11 12:14:17 +0100

    get blog url

--- blogs.py
+++ blogs.py
@@ -5,11 +5,11 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def parse_square_space_blogs(site_url: str, output_file: str = "blogs.json", verbose: bool = False):
+def parse_square_space_blogs(site_url: str, blog_subpage: str, output_file: str = "blogs.json", verbose: bool = False):
     if verbose:
-        print(f"Getting site {site_url}")
+        print(f"Getting site {site_url}{blog_subpage}")
 
-    site = BeautifulSoup(requests.get(site_url).text, "html.parser")
+    site = BeautifulSoup(requests.get(f"{site_url}{blog_subpage}").text, "html.parser")
 
     # Pages are broken up into sections with blogs
     sections = site.find(id="content").find_all(class_="sqs-gallery-design-list")
@@ -31,7 +31,9 @@ def parse_square_space_blogs(site_url: str, output_file: str = "blogs.json", ver
 
             blog_preview = blog.find(class_="summary-content")
 
-            title = blog_preview.find(class_="summary-title").text.strip()
+            title_element = blog_preview.find(class_="summary-title")
+            url = site_url + title_element.find("a")["href"][1:]
+            title = title_element.text.strip()
             parsed_blog["title"] = title
 
             date_element = blog_preview.find(class_="summary-metadata-item--date")
@@ -59,10 +61,11 @@ def main():
     parser = argparse.ArgumentParser("Extract squarespace blogs into json")
     parser.add_argument("-o", "--output", type=str, default="blogs.json", help="Output json file for blogs")
     parser.add_argument("-s", "--site", type=str, required=True, help="Site url")
+    parser.add_argument("-b", "--blog", type=str, required=True, help="Site subpage, so complete link is [SITE][BLOG]")
     parser.add_argument("-v", "--verbose", action="store_true", help="Logging")
     args = parser.parse_args()
 
-    parse_square_space_blogs(site_url=args.site, output_file=args.output, verbose=args.verbose)
+    parse_square_space_blogs(site_url=args.site, blog_subpage=args.blog, output_file=args.output, verbose=args.verbose)
 
 
 if __name__ == "__main__":
